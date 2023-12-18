edca67f919ac0e6f8d29ad91f6a28998f13ccecf
Adarsh Shukla <100258853+adarshpremshukla@users.noreply.github.com>
2023-12-18 18:27:12 +0530

    Update script.js

--- contentManagementTool/script.js
+++ contentManagementTool/script.js
@@ -1,24 +1,51 @@
-function addBlog() {
-    // Retrieve input values
-    var title = document.getElementById("title").value;
-    var image = document.getElementById("image").value;
-    var content = document.getElementById("content").value;
 
-    // Create HTML structure for the blog post
-    var blogPost = document.createElement("div");
-    blogPost.className = "blog-post";
-    blogPost.innerHTML = `
-      <h3>${title}</h3>
-      <img src="${image}" alt="Blog Image">
-      <p>${content}</p>
-    `;
+function submitForm() {
+  var title = document.getElementById('title').value;
+  var content = document.getElementById('content').value;
+  var image = document.getElementById('image').files[0];
+  var video = document.getElementById('video').files[0];
 
-    // Add the blog post to the preview section
-    var previewSection = document.getElementById("preview");
-    previewSection.appendChild(blogPost);
+  var dataSection = document.getElementById('dataSection');
 
-    // Clear input fields after adding the blog
-    document.getElementById("title").value = "";
-    document.getElementById("image").value = "";
-    document.getElementById("content").value = "";
+  var newData = "<div class='card'>" +
+    "<div class='card-header'>" +
+    "<h3 class='card-title'><b>Title:</b>" + title + "</h3>" +
+    "</div>" +
+    "<div class='card-body'>" +
+    "<p><b>Content: </b>" + content + "</p>";
+
+  if (image) {
+    var imagePreview = document.createElement('img');
+    imagePreview.src = URL.createObjectURL(image);
+    imagePreview.style.maxWidth = '100%';
+    imagePreview.style.height = 'auto';
+    imagePreview.style.marginTop = '10px';
+    newData += "<p><b>Image:</b></p>";
+    newData += "<div class='card-body-image'>" +
+      "<img class='preview' src='" + imagePreview.src + "' alt='Selected Image'>" +
+      "</div>";
   }
+  
+  if (video) {
+    var videoPreview = document.createElement('video');
+    videoPreview.src = URL.createObjectURL(video);
+    videoPreview.controls = true;
+    videoPreview.autoplay = true;
+    videoPreview.style.maxWidth = '100%';
+    videoPreview.style.height = 'auto';
+    videoPreview.style.marginTop = '20px';
+    newData += "<p><b>Video:</b></p>";
+    newData += "<div class='card-body-video'>" +
+      "<video class='video-preview' src='" + videoPreview.src + "' controls autoplay></video>" +
+      "</div>";
+  }
+
+  newData += "</div></div>";
+
+  dataSection.innerHTML += newData;
+
+  document.getElementById('title').value = '';
+  document.getElementById('content').value = '';
+  document.getElementById('image').value = '';
+  document.getElementById('video').value = '';
+}
